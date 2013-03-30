bc69ba44afa4b667d74b2fe654c81a0559167fa9
John Clements <clements@racket-lang.org>
2013-03-29 19:25:56 -0700

    start to abstract test-driver over prog- and tt-based-parsing

--- test-driver.rkt
+++ test-driver.rkt
@@ -14,28 +14,38 @@
         ;; kills the default jvm heap, sadly:
         #rx"run-pass/deep-vector2.rs$"))
 
-(time
- (for/sum ([f (in-directory "/Users/clements/tryrust")]
-           #:when (regexp-match #px".*\\.rs$" f)
-           ;;#:unless (ormap (lambda (pat) (regexp-match pat f)) lexer-dont-try-list)
-           )
-   (match-define (list _1 stdin _2 stderr control)
-       (process/ports 
-        dev-null
-        #f
-        #f
-        (let ([ans (~a "java org.antlr.v4.runtime.misc.TestRig Rust prog -tree -encoding UTF-8 "
-                       f)])
-          (printf "~s\n" ans)
-          ans)))
-   (close-output-port stdin)
-   (control 'wait)
-   (define result (control 'status))
-   (define errtext (first (regexp-match #px".*" stderr)))
-   (close-input-port stderr)
-   (when (or (not (eq? 'done-ok result))
-             (not (equal? errtext #"")))
-     (error 
-      (format "test of file ~s failed with result ~s and stderr\n~a"
-              f result errtext)))
+(define parser-dont-try-list
+  ;; just too rotted...
+  (list #rx"src/libfuzzer/"))
+
+(define (run-tests directory skip-list nonterm)
+(for/sum ([f (in-directory directory)]
+          #:when (regexp-match #px".*\\.rs$" f)
+          #:unless (ormap (lambda (pat) (regexp-match pat f))
+                          skip-list)
+          )
+  (match-define (list _1 stdin _2 stderr control)
+    (process/ports 
+     dev-null
+     #f
+     #f
+     (let ([ans (~a "java org.antlr.v4.runtime.misc.TestRig Rust "
+                    nonterm" -tree -encoding UTF-8 "
+                    f)])
+       (printf "~s\n" ans)
+       ans)))
+  (close-output-port stdin)
+  (control 'wait)
+  (define result (control 'status))
+  (define errtext (first (regexp-match #px".*" stderr)))
+  (close-input-port stderr)
+  (when (or (not (eq? 'done-ok result))
+            (not (equal? errtext #"")))
+    (error 
+     (format "test of file ~s failed with result ~s and stderr\n~a"
+             f result errtext)))
   1))
+
+(run-tests "/Users/clements/tryrust"
+           parser-dont-try-list
+           "prog")
